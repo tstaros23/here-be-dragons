6631ced42854d52b1f4eda6796029ba820029a88
Ted Staros <tstaros23@gmail.com>
2021-11-24 15:06:42 -0700

    Finish first test to pass

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -13,11 +13,14 @@ class NytimesTest < Minitest::Test
   def test_it_can_get_copyright
     #Set your code to the local variable, "result"
 
+    result = @hash[:copyright]
+
     assert result, "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
 
   def test_it_can_get_array_of_stories
     #Set your code to the local variable, "result"
+    
 
     assert result.is_a? (Array)
     assert_equal 44, result.count
